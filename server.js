123930a10891297dea70e26acf003c61f0405868
Brian Hudson <brianjasonhudson@gmail.com>
2017-06-20 02:42:51 -0500

    Added credentials

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(session({
    resave: false,
    saveUninitialized: false
 }))
-app.use(cors({origin: ['brianjhudson.com', 'github.com']}))
+app.use(cors({credentials: true, origin: ['brianjhudson.com', 'github.com']}))
 app.use(json())
 
 app.use(passport.initialize());
