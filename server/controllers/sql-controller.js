da53c5d5609c4762fe1dd0a7ff2c921053e564b8
Brian Hudson <brianjasonhudson@gmail.com>
2017-07-09 15:51:21 -0500

    Added query history

--- server/controllers/sql-controller.js
+++ server/controllers/sql-controller.js
@@ -3,7 +3,8 @@ const _ = require('underscore')
 
 module.exports = {
    getTables,
-   executeQuery
+   executeQuery,
+   getQueryHistory
 }
 
 function getTables(req, res, next) {
@@ -19,6 +20,22 @@ function getTables(req, res, next) {
    })
 }
 
+function getQueryHistory(req, res, next) {
+   if (req.user && req.user.id) {
+      knex('private.query_history')
+      .where({user_id: req.user.id})
+      .select('query_text')
+      .then(results => {
+         res.status(200).json(results)
+      })
+      .catch(err => {
+         res.status(500).json(err)
+      })
+   } else {
+      res.status(200).json([])
+   }
+}
+
 function executeQuery(req, res, next) {
    let forbiddenCommands = ['createuser', 'database', 'role', 'private']
    if (!req.user || !req.user.recognized) {
@@ -27,14 +44,22 @@ function executeQuery(req, res, next) {
    const query = req.body.query.toLowerCase()
    for (let cmd of forbiddenCommands) {
       if (query.includes(cmd)) {
-         let message = cmd.toUpperCase() + " is a forbidden command"
+         let message = cmd.toUpperCase() + " is a forbidden command; contact Brian for greater access"
          let position = query.indexOf(cmd)
          return res.status(200).json({error: true, message, position})
       }
    }
    knex.raw(req.body.query)
    .then(results => {
-      res.send(results)
+      req.user = {id: 1}
+      if (req.user && req.user.id) {
+         knex('private.query_history').insert({user_id: req.user.id, query_text: req.body.query, created_at: new Date()})
+         .then(response => {
+            res.status(200).send(results)
+         })
+      } else {
+         res.status(200).send(results)
+      }
    })
    .catch(err => {
       return res.status(201).json({error: true, message: err.message, position: err.position})
--- server/routes/sql-routes.js
+++ server/routes/sql-routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const sqlController = require('../controllers/sql-controller')
 
 router.get('/table', sqlController.getTables)
+router.get('/history', sqlController.getQueryHistory)
 router.post('/query', sqlController.executeQuery)
 
 module.exports = router
