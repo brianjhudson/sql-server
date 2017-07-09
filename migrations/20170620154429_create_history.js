84635aaf28e179177eebe1deecbd407cb0332925
Brian Hudson <brianjasonhudson@gmail.com>
2017-07-09 15:36:58 -0500

    Removed query_date (already in timestamp)

--- migrations/20170620154429_create_history.js
+++ migrations/20170620154429_create_history.js
@@ -3,7 +3,6 @@ exports.up = function(knex, Promise) {
   return knex.schema.withSchema('private').createTableIfNotExists('query_history', table => {
       table.increments()
       table.integer('user_id')
-      table.timestamp('query_date')
       table.string('query_text')
       table.timestamps()
       table.foreign('user_id').references('id').inTable('private.user_account')
